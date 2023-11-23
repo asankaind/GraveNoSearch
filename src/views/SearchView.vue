73e2e431817acf332b7d439026c4d714a0fcddb3
EMBLA\ThilankaK <thilankak@embla.asia>
2023-11-23 15:28:59 +0530

    search ui

---
+++ src/views/SearchView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AgGravSearchComponent from "../components/search/AgGravSearchComponent.vue";
+</script>
+
+<template>
+  <div >
+    <ag-grav-search-component />
+  </div>
+</template>
